18261bb059ee328520ae221ca581ac463fa18237
Andrew <arcabezudo@dons.usfca.edu>
2022-08-24 22:15:07 -0700

    Complete exercises 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,44 +1,81 @@
 import { useState } from 'react'
 
+const FilterForm = (props) => {
+  return (
+    <form>
+      <div>filter shown with <input value={props.value} onChange={props.onChange}/></div>
+    </form>
+  )
+}
+
 const Entry = (props) => {
   
   return (
     <dt>
-    {props.name}
+    {props.name} {props.num}
     </dt>
+  )
+}
+
+const PersonForm = (props) => {
+  return (
+    <form onSubmit={props.onSubmit}>
+      <div>name: <input value={props.value} onChange={props.onChange}/></div>
+      <div>number: <input value={props.value2} onChange={props.onChange2}/></div>
+      <div>
+          <button type='submit'>add</button>
+        </div>
+    </form>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchEntry, setSearchEntry] = useState('')
 
-  const addName = (event) => {
+
+  const addPerson = (event) => {
     event.preventDefault()
-    const nameObject = {name: newName}
-    setPersons(persons.concat(nameObject))
+
+    if (persons.some(person => person.name === newName)) {
+      window.alert(`${newName} is already added to the phonebook`)
+    } else {
+      const nameObject = {name: newName, number: newNumber}
+      setPersons(persons.concat(nameObject))
+    }
   }
   
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  const handleSearch = (event) => {
+    setSearchEntry(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          <button type='submit'>add</button>
-        </div>
-      </form>
+      <FilterForm value={searchEntry} onChange={handleSearch}/>
+      
+      <h2>add a new</h2>
+      <PersonForm onSubmit={addPerson} value={newName} onChange={handleNameChange}
+        value2={newNumber} onChange2={handleNumberChange}/>
+      
       <h2>Numbers</h2>
+      
       <dl>
-        {persons.map(person =>
-          <Entry key={person.name} name={person.name} />
+        {persons.filter(person => person.name.toLowerCase().includes(searchEntry.toLowerCase())).map(person =>
+          <Entry key={person.name} name={person.name} num={person.number}/>
          )}
       </dl>
     </div>
